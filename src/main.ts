3f7c4639ad51d4dd5b032296c82ca4236b45ec00
wends05 <cxnner05@gmail.com>
2024-09-01 15:17:16 +0800

    changed some styles

--- src/main.ts
+++ src/main.ts
@@ -24,9 +24,11 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = /*html*/ `
 
 const submitItem = (todo: Todo) => {
 
+  const idString = todo.id.toString()
+
   const todoElement = document.createElement('li')
   const checkBox = document.createElement('input')
-  const label = document.createElement('p')
+  const label = document.createElement('label')
   const button = document.createElement("button")
   const trash = document.createElement("img")
 
@@ -37,10 +39,12 @@ const submitItem = (todo: Todo) => {
   button.appendChild(trash)
 
   checkBox.type = 'checkbox'
-  checkBox.id = todo.id.toString()
+  label.setAttribute("name", idString)
+  checkBox.id = idString
   checkBox.checked = todo.isComplete
 
   label.setAttribute("class", "todo-label")
+  label.setAttribute("for", idString)
   label.innerHTML = todo.name
   todoElement.setAttribute("class", "todo")
   todoElement.append(checkBox, label, button)
